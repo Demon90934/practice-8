391c7f178f2be4b597ec521641f9b584d4443d9d
Demon90934 <91471287+Demon90934@users.noreply.github.com>
2021-12-17 19:16:03 +0500

    Add files via upload

---
+++ lab8.py
@@ -0,0 +1,75 @@
+﻿from random import randint
+import datetime
+from prettytable import PrettyTable
+import sorts
+
+
+def inp():  # Считывание количества элементов в последовательности
+    number = int(input("Введите число элементов последовательности:"))
+    return number
+
+
+def create_sequence():  # Создание последовательности
+    list_n = []
+    for i in range(0, n+1):
+        list_n.append(randint(-5678, 45672))
+    return list_n
+
+
+def out_check():
+    my_file = open("output.txt", "w")
+    my_file.write(str(sorts.check(sequence)) + "\n")
+    my_file.close()
+    pass
+
+
+def sorting_time(s):
+    time_1 = datetime.datetime.now()
+    sorts.bubble_sorting(s)
+    time_bubble = datetime.datetime.now() - time_1
+
+    time_2 = datetime.datetime.now()
+    sorts.sorting_choice(s)
+    time_choice = datetime.datetime.now() - time_2
+
+    time_3 = datetime.datetime.now()
+    sorts.quick_sorting(s)
+    time_quick = datetime.datetime.now() - time_3
+
+    time_4 = datetime.datetime.now()
+    sorted(s)
+    time_sorted = datetime.datetime.now() - time_4
+    return time_bubble, time_choice, time_quick, time_sorted
+
+
+def add_table():
+    table_sorts = PrettyTable()
+    table_sorts.add_column("Метод сортировки", ["Сортировка пузырьком", "Сортировка выбором", "Быстрая сортировка",
+                                                "Встроенная сортировка"])
+    table_sorts.add_column("Рандомная посл-ть", [time_bubble_seq, time_choice_seq, time_quick_seq, time_sorted_seq])
+    table_sorts.add_column("Отсортированная посл-ть", [time_bubble_s_seq, time_choice_s_seq, time_quick_s_seq,
+                                                       time_sorted_s_seq])
+    table_sorts.add_column("Отсортированная в обратном порядке посл-ть", [time_bubble_r_seq, time_choice_r_seq,
+                                                                          time_quick_r_seq, time_sorted_r_seq])
+    return table_sorts
+
+
+def out():
+    my_file = open("output.txt", "a")
+    my_file.write("Количество элементов в последовательности: %d" % n + "\n")
+    my_file.write(str(sequence) + "\n")
+    my_file.write(str(sort_table))
+    my_file.close()
+    pass
+
+
+n = inp()
+sequence = create_sequence()
+out_check()
+sort_sequence = sorted(sequence)
+reverse_sequence = sorted(sequence, reverse=True)
+time_bubble_seq, time_choice_seq, time_quick_seq, time_sorted_seq = sorting_time(sequence)
+time_bubble_s_seq, time_choice_s_seq, time_quick_s_seq, time_sorted_s_seq = sorting_time(sort_sequence)
+time_bubble_r_seq, time_choice_r_seq, time_quick_r_seq, time_sorted_r_seq = sorting_time(reverse_sequence)
+sort_table = add_table()
+out()
---
+++ sorts.py
@@ -0,0 +1,49 @@
+﻿import random
+
+
+def bubble_sorting(a):
+    for j in range(0, len(a) - 1):
+        for k in range(0, len(a) - 1):
+            if a[k] > a[k+1]:
+                a[k] = a[k+1]
+                z = a[k+1]
+                a[k+1] = z
+    return a
+
+
+def sorting_choice(b):
+    for i in range(0, len(b) - 1):
+        min_element = b[i]
+        k = i
+        for j in range(i+1, len(b)):
+            if b[j] < min_element:
+                k = j
+                min_element = b[j]
+        b[k] = b[i]
+        b[i] = min_element
+    return b
+
+
+def quick_sorting(c):
+    if len(c) <= 1:
+        return c
+    else:
+        element = random.choice(c)
+        c_1 = []
+        c_2 = []
+        c_3 = []
+        for i in c:
+            if i < element:
+                c_1.append(i)
+            elif i > element:
+                c_2.append(i)
+            else:
+                c_3.append(i)
+        return quick_sorting(c_1) + c_3 + quick_sorting(c_2)
+
+
+def check(d):
+    for i in range(1, len(d)):
+        if d[i - 1] > d[i]:
+            return False
+    return True
